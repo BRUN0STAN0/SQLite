1486b75dac2ee364d0c64a86c601447a112688d2
Bruno Stano <brunostano00@protonmail.com>
2023-11-09 11:33:22 +0100

    Add project files.

---
+++ SQLite/Controllers/CharactersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SQLite.Data;
+using SQLite.Models;
+
+namespace SQLite.Controllers
+{
+    public class CharactersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CharactersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var characters = _context.RpgCharacters;
+            return View(characters);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(RpgCharacter model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.RpgCharacters.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ SQLite/Migrations/20231109102551_MigrationEdit.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SQLite.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationEdit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RpgCharactercs");
+
+            migrationBuilder.CreateTable(
+                name: "RpgCharacters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    RpgClass = table.Column<string>(type: "TEXT", nullable: false),
+                    HitPoints = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RpgCharacters", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RpgCharacters");
+
+            migrationBuilder.CreateTable(
+                name: "RpgCharactercs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    HitPoints = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    RpgClass = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RpgCharactercs", x => x.Id);
+                });
+        }
+    }
+}
